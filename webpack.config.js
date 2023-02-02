ce75c9ef89cb674498edc5cb898eabf82f18361e
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-02-02 10:50:51 -0500

    Lee html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // leer html
 module.expors = {
   entry: "./src/index.js",
   output: { path: path.resolve(__dirname, "dist"), filename: "bundle.js" },
@@ -11,7 +12,27 @@ module.expors = {
       //quiero que lea extensiones de js y jsx
       {
         test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      //leer html
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
