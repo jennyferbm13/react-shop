253b3cba53482bea0e81c991af1333cda4e8aed8
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-02-03 15:17:35 -0500

    Configuracion basica

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,31 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // leer html
-module.expors = {
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
   entry: "./src/index.js",
   output: { path: path.resolve(__dirname, "dist"), filename: "bundle.js" },
-
-  resolve: {
-    extensions: { extensions: [".js", ".jsx"] },
-  },
+  mode: "development",
+  resolve: { extensions: [".js", ".jsx"] },
   module: {
     rules: [
-      //quiero que lea extensiones de js y jsx
       {
         test: /\.(js|jsx)$/,
-        exclude: "/node_modules/",
-        use: {
-          loader: "babel-loader",
-        },
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
       },
-      //leer html
+
+      { test: /\.html$/, use: [{ loader: "html-loader" }] },
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      inject: true,
       template: "./public/index.html",
       filename: "./index.html",
     }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
   ],
 };
