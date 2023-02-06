d7ad436a051a2e790b3fe1be86ca82a93a6314b8
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-02-06 15:03:27 -0500

    Creacion pagina NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import "../style/NotFound.scss";
 const RecoveryPassword = () => {
-  return <div>No found</div>;
+  return (
+    <div className="login">
+      <div className="form-container">
+        <h1 className="titleFound">404</h1>
+        <p className="subtitleFound">
+          <strong>Not Found</strong>
+        </p>
+        <p className="subtitleFound">
+          The resource requested could no be found on this server
+        </p>
+        <input
+          type="submit"
+          value="Go back"
+          className="primary-button login-button"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RecoveryPassword;
