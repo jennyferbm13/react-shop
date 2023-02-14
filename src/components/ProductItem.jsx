86812581e225bb315f7195cc679a09a60fa1ff1c
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-02-14 14:47:30 -0500

    customHooks

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,12 +3,13 @@ import "@style/ProductItem.scss";
 
 import addCart from "@icons/bt_add_to_cart.svg";
 const ProductItem = ({ product }) => {
+  const { images, title, price } = product;
   return (
     <div className="product-card">
       <img src={product.images} alt={product.title} />
       <div className="product-info">
         <div>
-          <p>${product.price}</p>
+          <p>$ {price}</p>
           <p>{product.title}</p>
         </div>
         <figure>
